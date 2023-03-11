a44c0a133ba7e24284333f41a353842259927c5f
ezz2003 <ezz2003@yandex.ru>
2023-03-11 20:11:59 +1000

    solved the problem54

---
+++ problem54/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] MakeMatrixIntRnd(int rows, int columns, int min, int max)
+{
+  int[,] matrix = new int[rows, columns];
+  Random rnd = new Random();
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+    {
+      Console.Write($"{matrix[i, j],4},");
+    }
+    Console.WriteLine($"{matrix[i, matrix.GetLength(1) - 1],4}");
+  }
+}
+
+void ReversSortRowsMatrix(ref int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1) - 1; j++)
+    {
+      int maxIndex = j;
+      for (int l = j + 1; l < matr.GetLength(1); l++)
+      {
+        if (matr[i, l] > matr[i, maxIndex])
+        {
+          maxIndex = l;
+        }
+      }
+      (matr[i, j], matr[i, maxIndex]) = (matr[i, maxIndex], matr[i, j]);
+    }
+  }
+}
+
+
+
+
+int rowsMatrix = InputInt("Введите количество строк в матрице");
+int columnsMatrix = InputInt("Введите количество столбцов в матрице");
+int[,] readyMatrix = MakeMatrixIntRnd(rowsMatrix, columnsMatrix, 1, 10);
+PrintMatrix(readyMatrix);
+Console.WriteLine();
+ReversSortRowsMatrix(ref readyMatrix);
+PrintMatrix(readyMatrix);
